cf88bb555ad1be2a54019f96b51137d8ed054307
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-10 08:45:55 +0100

    Creating the FeedBack component

---
+++ src/components/FeedBackItem.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+const FeedBackItem = () => {
+	return (
+		<div className='card'>
+			<div className='num-display'>10</div>
+			<div className='text-display'>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam
+				consectetur, aut reiciendis nemo ea sit.
+			</div>
+		</div>
+	)
+}
+
+export default FeedBackItem
