f3e45ed3f468af4570d4ad8072bbfc77b67fcaf6
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-10 21:51:17 +0100

    Updating the Button & Adding the input validations

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,10 +1,23 @@
 import Card from './shared/Card'
 import { useState } from 'react'
+import Button from './shared/Button'
 
 const FeedbackForm = () => {
 	const [text, setText] = useState('')
+	const [btnDisabled, setBtnDisabled] = useState(true)
+	const [message, setMessage] = useState('')
 
 	const handleTextChange = (e) => {
+		if (text === '') {
+			setBtnDisabled(true)
+			setMessage(null)
+		} else if (text !== '' && text.trim().length <= 10) {
+			setBtnDisabled(true)
+			setMessage('Text must be al least 10 characters long')
+		} else {
+			setBtnDisabled(false)
+			setMessage(null)
+		}
 		setText(e.target.value)
 	}
 
@@ -19,8 +32,11 @@ const FeedbackForm = () => {
 						placeholder='Write a review'
 						value={text}
 					/>
-					<button type='submit'>Submit</button>
+					<Button type='submit' isDisabled={btnDisabled}>
+						Submit
+					</Button>
 				</div>
+				{message && <div className='message'>{message}</div>}
 			</form>
 		</Card>
 	)
