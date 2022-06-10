43d7ecb0d22098b920a3c6ead61ae33f1b7aba33
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-10 22:13:12 +0100

    Updating the RatingButtons component

--- src/components/RatingButtons.jsx
+++ src/components/RatingButtons.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+function RatingSelect({ select, selected }) {
+	const handleChange = (e) => {
+		select(+e.currentTarget.value)
+	}
 
-const RatingButtons = () => {
-	return <div>RatingButtons</div>
+	return (
+		<ul className='rating'>
+			{Array.from({ length: 10 }, (_, i) => (
+				<li key={`rating-${i + 1}`}>
+					<input
+						type='radio'
+						id={`num${i + 1}`}
+						name='rating'
+						value={i + 1}
+						onChange={handleChange}
+						checked={selected === i + 1}
+					/>
+					<label htmlFor={`num${i + 1}`}>{i + 1}</label>
+				</li>
+			))}
+		</ul>
+	)
 }
 
-export default RatingButtons
+export default RatingSelect
