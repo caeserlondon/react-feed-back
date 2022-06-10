a7e2627faec6d628cd6e520ae6dbe0a32b08f3e3
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-10 08:45:23 +0100

    Creating the Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+	return (
+		<header>
+			<div className='container'>
+				<h2>Feed Back Interface</h2>
+			</div>
+		</header>
+	)
+}
+
+export default Header
