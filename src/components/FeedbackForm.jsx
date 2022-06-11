7f4c49b823babfe8c938f068d3fa2198cf481d26
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-11 08:31:18 +0100

    Adding the select function as a props to the RatingButtons component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -7,6 +7,7 @@ const FeedbackForm = () => {
 	const [text, setText] = useState('')
 	const [btnDisabled, setBtnDisabled] = useState(true)
 	const [message, setMessage] = useState('')
+	const [rating, setRating] = useState(10)
 
 	const handleTextChange = (e) => {
 		if (text === '') {
@@ -26,7 +27,7 @@ const FeedbackForm = () => {
 		<Card>
 			<form>
 				<h2>How would you rate our service?</h2>
-				<RatingButtons />
+				<RatingButtons select={(rating) => setRating(rating)} />
 				<div className='input-group'>
 					<input
 						onChange={handleTextChange}
