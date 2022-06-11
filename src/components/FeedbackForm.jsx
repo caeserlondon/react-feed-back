c4dcdf8d68fb216b28662de72e092f256346f376
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-11 21:41:22 +0100

    Adding the edit functunality

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,5 +1,5 @@
 import Card from './shared/Card'
-import { useState, useContext } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import Button from './shared/Button'
 import RatingButtons from './RatingButtons'
 import FeedbackContext from './context/FeedbackContext'
@@ -8,8 +8,18 @@ const FeedbackForm = () => {
 	const [text, setText] = useState('')
 	const [btnDisabled, setBtnDisabled] = useState(true)
 	const [message, setMessage] = useState('')
-	const [rating, setRating] = useState(10)
-	const { addFeedback } = useContext(FeedbackContext)
+	const [rating, setRating] = useState()
+
+	const { addFeedback, feedbackEdit, updateFeedback } =
+		useContext(FeedbackContext)
+
+	useEffect(() => {
+		if (feedbackEdit.edit === true) {
+			setBtnDisabled(false)
+			setText(feedbackEdit.item.text)
+			setRating(feedbackEdit.item.rating)
+		}
+	}, [feedbackEdit])
 
 	const handleTextChange = (e) => {
 		if (text === '') {
@@ -32,8 +42,13 @@ const FeedbackForm = () => {
 				text,
 				rating,
 			}
-			addFeedback(newFeedback)
+			if (feedbackEdit.edit === true) {
+				updateFeedback(feedbackEdit.item.id, newFeedback)
+			} else {
+				addFeedback(newFeedback)
+			}
 			setText('')
+			setBtnDisabled(true)
 		}
 	}
 
@@ -43,7 +58,7 @@ const FeedbackForm = () => {
 				<h2>How would you rate our service?</h2>
 				<RatingButtons
 					select={(rating) => setRating(rating)}
-					selected={rating}
+					// selected={rating}
 				/>
 				<div className='input-group'>
 					<input
