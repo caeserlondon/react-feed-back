d62df9a3a0a38aa8dab33a144d4176cd3b1f5f1f
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-11 21:40:30 +0100

    Adding the useContext and edit functunality

--- src/components/FeedBackItem.jsx
+++ src/components/FeedBackItem.jsx
@@ -1,15 +1,18 @@
 import Card from './shared/Card'
-import { FaTimes } from 'react-icons/fa'
+import { FaTimes, FaEdit } from 'react-icons/fa'
 import { useContext } from 'react'
 import FeedbackContext from './context/FeedbackContext'
 
 const FeedbackItem = ({ item }) => {
-	const { deleteFeedback } = useContext(FeedbackContext)
+	const { deleteFeedback, editFeedback } = useContext(FeedbackContext)
 	return (
 		<Card>
 			<div className='num-display'>{item.rating}</div>
 			<button className='close' onClick={() => deleteFeedback(item.id)}>
 				<FaTimes />
+			</button>
+			<button className='edit' onClick={() => editFeedback(item)}>
+				<FaEdit />
 			</button>
 			<div className='text-display'>{item.text}</div>
 		</Card>
