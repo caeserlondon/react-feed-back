4d03fd4cfd979ee563544fc5678f4c07dea7d32c
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-12 18:25:00 +0100

    Adding the loading spinner

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,15 +2,18 @@
 import FeedbackItem from './FeedbackItem'
 import { useContext } from 'react'
 import FeedbackContext from './context/FeedbackContext'
+import Spinner from './shared/Spinner'
 
 const FeedbackList = () => {
-	const { feedback } = useContext(FeedbackContext)
+	const { feedback, isLoading } = useContext(FeedbackContext)
 
-	if (!feedback || feedback.length === 0) {
+	if (!isLoading && (!feedback || feedback.length === 0)) {
 		return <h1 style={{ color: 'darkorange' }}>No Feedback Yet</h1>
 	}
 
-	return (
+	return isLoading ? (
+		<Spinner />
+	) : (
 		<div className='feedback-list'>
 			{feedback.map((item) => (
 				<FeedbackItem key={item.id} item={item} />
